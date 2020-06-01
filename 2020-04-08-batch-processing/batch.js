3b46f56c0b54f2ae8b90dfc1b285914b7dfc91a2
Xiao Shen <xiaoshen@stripe.com>
2020-06-01 19:06:10 -0400

    making refund apis in batch

--- 2020-04-08-batch-processing/batch.js
+++ 2020-04-08-batch-processing/batch.js
@@ -20,14 +20,22 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 // note: take your own live traffic to Stripe into account when calculating a reasonable limit.
 const limit = RateLimit(20);
 
-const processLine = async (line) => {
+const processLine = async (charge_id) => {
   try {
     // ensure we are able to operate within set limit first
     await limit();
-    console.log(`processing ${line}`);
+    console.log(`processing ${charge_id}`);
+
+    // make the refund
+    await stripe.refunds.create({
+      charge: charge_id,
+      refund_application_fee: true,
+      reverse_transfer: true 
+    })
+
     // stripe call here
   } catch (error) {
-    writeFailureToFile(line, error);
+    writeFailureToFile(charge_id, error);
     // we recommend also checking the error.code for `rate_limit` value -
     // this allows you to add additional logic to handle retrying
     // see https://stripe.com/docs/error-handling and also https://stripe.com/docs/error-codes
@@ -37,16 +45,20 @@ const processLine = async (line) => {
 // log which operations failed and why
 // note: using our webhooks is also a great way to track operation failures.
 // see https://stripe.com/docs/webhooks for more
-const writeFailureToFile = (line, error) => {
+const writeFailureToFile = (charge_id, error) => {
   try {
-    fs.appendFileSync('processing_failures.txt', `${line} ${error.code}\n`);
+    fs.appendFileSync('processing_failures.txt', `${charge_id} ${error.code}\n`);
   } catch (error) {
     throw error;
   }
 };
 
+
+// update file name here
 const reader = readline.createInterface({
-  input: fs.createReadStream('ids.csv')
+  input: fs.createReadStream('Tiny_Ninjas__Heroes_6-1-20-2020-06-01-20-38-10-charges.csv')
+  // input: fs.createReadStream('refund_test.csv')
+
 });
 
 reader.on('line', processLine);
